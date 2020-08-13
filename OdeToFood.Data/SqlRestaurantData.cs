5bc81e3f7a101870a9b68b1040debface88636a2
scottgulbin <sc.gulbin@gmail.com>
2020-08-13 16:37:29 -0400

    Added more ui functionality in the _Layout view and added additional views to insert into other razor pages.

--- OdeToFood.Data/SqlRestaurantData.cs
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -42,6 +42,11 @@ namespace OdeToFood.Data
             return restaurant;
         }
 
+        public int GetCountOfRestaurants()
+        {
+            return db.Restaurants.Count();
+        }
+
         public IEnumerable<Restaurant> GetRestaurantsByName(string name)
         {
             var query = from r in db.Restaurants
